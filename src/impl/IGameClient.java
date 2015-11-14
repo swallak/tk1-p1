0fe0d1ce87b7d2a6faa19ef6a3fcd4f22dabdff4
Saad Ouassil Allak <sw.allak@gmail.com>
2015-11-14 09:13:32 +0100

    Delete originl interfaces

--- src/impl/IGameClient.java
+++
@@ -1,9 +0,0 @@
-package impl;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IGameClient extends Remote{
-	void recieveFlyHunted(String playerName, int newPoints) throws RemoteException;
-	void recieveFlyPosition(int x, int y) throws RemoteException;
-}
